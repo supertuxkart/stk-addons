8bd12dd20bdb30e460eaefe130514d3c34c4d2ef
xapantu <xapantu@178a84e3-b1eb-0310-8ba1-8eac791a3b58>
2010-01-31 10:44:45 +0000

    (stkaddons) Adding credits page

--- about.php
+++ about.php
@@ -20,47 +20,30 @@
 /***************************************************************************
 Project: STK Addon Manager
 
-File: menu.php
+File: about.php
 Version: 1
 Licence: GPLv3
-Description: menu
+Description: credits page
 
 ***************************************************************************/
 
+$security ="";
+include("include/security.php");
+include("include/top.php");
+include("include/config.php");
 ?>
-
-<div id="global">
-<div id="top">
-	<?php
-	if(isset($_SESSION["login"]))
-	{
-		echo _("Welcome")." ".$_SESSION["login"];
-	}
-	echo '<a href="index.php">';
-	echo _("Home");
-	echo '</a>';
-	if(isset($_SESSION["login"]))
-	{
-		echo'<a href="unlogin.php">'._("Log out").'</a>';
-		echo'<a href="manageAccount.php">'._("Users").'</a>';
-		echo'<a href="upload.php">'._("Upload").'</a>';
-	}
-	else
-	{
-		
-	echo'<a href="login.php">';
-	echo _('Login');
-	echo '</a>';
-	}
-	//echo '<a href="upload.php">'._("Upload")'.</a>';
-	 ?>
-	 <a href="about.php">About</a>
-	     <div class="container">
-<a class="menu_head"><?php echo _("Languages");?></a>
-<ul class="menu_body">
-<li><a href="<?php echo $nom_page.'&amp;lang=nl'; ?>"><img src="image/flag/nl.png" /></a></li>
-<li><a href="<?php echo $nom_page.'&amp;lang=fr'; ?>"><img src="image/flag/fr.png" /></a></li>
-<li><a href="<?php echo $nom_page.'&amp;lang=en'; ?>"><img src="image/flag/en.png" /></a></li>
-</ul>
-</div>
-</div>
+	</head>
+	<body>
+		<?php 
+		include("menu.php");
+		?>
+		<div id="content">
+		<?php
+		include ("CREDITS");
+		?>
+		</div>
+		<?php
+		include("include/footer.php");
+		?>
+		</head>
+		</html>
