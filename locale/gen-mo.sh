3d1ab3d59ca967b54a608184033ab62ec9f8d456
stephenjust <stephenjust@178a84e3-b1eb-0310-8ba1-8eac791a3b58>
2011-07-10 04:32:59 +0000

    [stkaddons] Updated translations

--- locale/gen-mo.sh
+++ locale/gen-mo.sh
@@ -2,3 +2,4 @@ msgfmt translations-fr.po -o fr_FR/LC_MESSAGES/translations.mo
 msgfmt translations-ga.po -o ga_IE/LC_MESSAGES/translations.mo
 msgfmt translations-nl.po -o nl_NL/LC_MESSAGES/translations.mo
 msgfmt translations-de.po -o de_DE/LC_MESSAGES/translations.mo
+msgfmt translations-ru.po -o ru_RU/LC_MESSAGES/translations.mo
