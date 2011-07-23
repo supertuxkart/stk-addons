0db3102b2808ea47864138aef12dae8b76018a73
stephenjust <stephenjust@178a84e3-b1eb-0310-8ba1-8eac791a3b58>
2011-07-23 21:25:43 +0000

    [stkaddons] Removed outdated files

--- update.php
+++
@@ -1,8 +0,0 @@
-<?php
-include("include/connectMysql.php");
-            $propertie_sql = mysql_query("SELECT * FROM users ");
-            while($propertie = mysql_fetch_array($propertie_sql))
-            {
-            mysql_query("UPDATE `stkaddons_stkbase`.`users` SET `avatar` = '".$propertie['login'].".png' WHERE `users`.`id` =".$propertie['id']." LIMIT 1 ;")or die(mysql_error());
-            }
-            ?>
