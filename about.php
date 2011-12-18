5516303e45313e2148be9c2f6f4d50380aa2ec55
stephenjust <stephenjust@178a84e3-b1eb-0310-8ba1-8eac791a3b58>
2011-12-18 02:24:12 +0000

    [stkaddons] Added info about STK and TuxFamily to the about page.

--- about.php
+++ about.php
@@ -39,6 +39,19 @@ include('include/top.php');
 		include('include/menu.php');
 		?>
 		<div id="content">
+                    <h1>SuperTuxKart</h1>
+                    <p><?php echo htmlspecialchars(_('SuperTuxKart is a Free 3D kart racing game, with many tracks, characters and items for you to try.')); ?></p>
+                    <p><?php echo htmlspecialchars(_('Since version 0.7.1, SuperTuxKart has had the ability to fetch important messages from the STKAddons website. Since 0.7.2, the game has included a built-in add-on manager.').' ');
+                        echo sprintf(htmlspecialchars(_('SuperTuxKart now has over %d karts, %d tracks, and %d arenas available in-game thanks to the add-on service.')),40,30,15); ?></p>
+                    <p><?php echo htmlspecialchars(_('Of course, the artists who create all of this content must be thanked too. Without them, the add-on website would not be as great as it is today.')); ?></p>
+                    <p><a href="http://supertuxkart.sourceforge.net/"><?php echo htmlspecialchars(_('Website')); ?></a> | <a href="http://sourceforge.net/donate/index.php?group_id=202302"><?php echo htmlspecialchars(_('Donate!')); ?></a></p>
+                    
+                    <h1>TuxFamily</h1>
+                    <p><?php echo htmlspecialchars(_('TuxFamily is a non-profit organization. It provides free services for projects and contents dealing with the free software philosophy (free as in free speech, not as in free beer). They accept any project released under a free license (GPL, BSD, CC-BY-SA, Art Libre...).')); ?></p>
+                    <p><?php echo htmlspecialchars(_('TuxFamily operates the servers on which STKAddons runs, for free. Because of them, we can provide the add-on service for SuperTuxKart. Each month, over a million downloads are made by SuperTuxKart players. We thank them very much for their generosity to us and to other open source projects.')); ?></p>
+                    <p><a href="http://tuxfamily.org/"><?php echo htmlspecialchars(_('Website')); ?></a> | <a href="http://tuxfamily.org/en/support"><?php echo htmlspecialchars(_('Donate!')); ?></a></p>
+                    
+                    <h1><?php echo htmlspecialchars(_('Credits')); ?></h1>
 		<?php
 		$credits = file_get_contents("CREDITS");
                 echo '<pre>'.$credits.'</pre>';
