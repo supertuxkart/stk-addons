95a8c066bc21d20e3cfef304a53a6ec69f398fc5
stephenjust <stephenjust@178a84e3-b1eb-0310-8ba1-8eac791a3b58>
2011-12-28 21:44:52 +0000

    [stkaddons] Replaced line graph in client report with two separate graphs - one for version, and one for OS. It's a little easier on the eyes.

--- reports/clients.php
+++ reports/clients.php
@@ -28,12 +28,29 @@ $description = '<p>This report is designed to give day-to-day usage statistics, 
     'using download counts, collected daily.</p>';
 $report->addDescription($description);
 
+$uaVer1y_section = $report->addSection("File Downloads per Version in the Last Year");
+$uaVer1y_query = 'SELECT `label`,`date`,SUM(`value`) FROM (
+        SELECT SUBSTRING_INDEX(SUBSTRING(`type`,21),\' \',1) AS `label`,`date`,`value`
+        FROM `'.DB_PREFIX.'stats`
+        WHERE `date` >= CURDATE() - INTERVAL 1 YEAR
+        AND `type` LIKE \'uagent %\'
+        ORDER BY `date` DESC
+    ) AS `t`
+    GROUP BY `t`.`date`,`t`.`label`
+    ORDER BY `t`.`date` DESC, `t`.`label` DESC';
+$report->addTimeGraph($uaVer1y_section,$uaVer1y_query, 'File Downloads per Version', 'ua_ver_1y');
+
 $uaTime1y_section = $report->addSection("File Downloads per OS in the Last Year");
-$uaTime1y_query = 'SELECT SUBSTRING(`type`,21) AS `label`,`date`,`value`
-    FROM `'.DB_PREFIX.'stats`
-    WHERE `date` >= CURDATE() - INTERVAL 1 YEAR
-    AND `type` LIKE \'uagent %\'
-    ORDER BY `date` DESC, `label` DESC';
+$uaTime1y_query = 'SELECT CASE WHEN `label` = \'\' THEN \'Unknown\' ELSE `label` END AS `label`,`date`,SUM(`value`) FROM (
+        SELECT TRIM(REPLACE(REPLACE(REPLACE(`type`,SUBSTRING_INDEX(`type`,\' \',2),\'\'),\')\',\'\'),\'(\',\'\'))
+        AS `label`,`date`,`value`
+        FROM `'.DB_PREFIX.'stats`
+        WHERE `date` >= CURDATE() - INTERVAL 1 YEAR
+        AND `type` LIKE \'uagent %\'
+        ORDER BY `date` DESC
+    ) AS `t`
+    GROUP BY `t`.`date`,`t`.`label`
+    ORDER BY `t`.`date` DESC, `t`.`label` DESC;';
 $report->addTimeGraph($uaTime1y_section,$uaTime1y_query, 'File Downloads per OS', 'ua_time_1y');
 
 $last30_section = $report->addSection("File Downloads in the Last 30 Days");
