94c2d857fe1489547081e2088db5107b1991bc64
stephenjust <stephenjust@178a84e3-b1eb-0310-8ba1-8eac791a3b58>
2013-08-19 01:30:42 +0000

    [stkaddons] Fix language parameter XSS issue

---
+++ include/SLocale.class.php
@@ -0,0 +1,103 @@
+<?php
+/**
+ * copyright 2013 Stephen Just <stephenjust@users.sf.net>
+ *
+ * This file is part of stkaddons
+ *
+ * stkaddons is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * stkaddons is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with stkaddons.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+// Note that PHP has a built-in Locale class in the newest versions of PHP
+class SLocale {
+    
+    /**
+     * Array of supported languages, format is:
+     * language code, flag image x-offset, flag image y-offset, flag label
+     * @var array
+     */
+    private static $languages = array(
+        array('en_US',0,0,'EN'),
+        array('ca_ES',-96,-99,'CA'),
+        array('de_DE',0,-33,'DE'),
+        array('es_ES',-96,-66,'ES'),
+        array('eu_ES', -144, -66, 'EU'),
+        array('fr_FR',0,-66,'FR'),
+        array('ga_IE',0,-99,'GA'),
+        array('gd_GB',-144, -33,'GD'),
+        array('gl_ES',-48,0,'GL'),
+        array('id_ID',-48,-33,'ID'),
+        array('it_IT',-96,-33,'IT'),
+        array('nl_NL',-48,-66,'NL'),
+        array('pt_BR',-144,0,'PT'),
+        array('ru_RU',-48,-99,'RU'),
+        array('zh_TW',-96,0,'ZH (T)')
+        );
+    
+    private static $cookie_lifetime = 31536000; // One year
+    
+    /**
+     * Create the locale object
+     * @param string $locale
+     */
+    public function __construct($locale = NULL) {
+        if ($locale == NULL && isset($_GET['lang']) && strlen($_GET['lang']) != 0) {
+            $locale = $_GET['lang'];
+        } elseif ($locale == NULL && isset($_COOKIE['lang'])) {
+            $locale = $_COOKIE['lang'];
+        } elseif ($locale == NULL)
+            $locale = "en_US";
+
+        if (!SLocale::IsValid($locale))
+            die("Invalid locale");
+        
+        SLocale::SetLocale($locale);
+    }
+    
+    /**
+     * Check if locale is a valid value
+     * @param string $locale
+     * @return boolean
+     */
+    public static function IsValid($locale) {
+        foreach (SLocale::$languages AS $lang) {
+            if ($locale == $lang[0]) return true;
+        }
+        return false;
+    }
+    
+    /**
+     * Set page locale
+     * @param string $locale Locale string - input should already be checked
+     */
+    private static function SetLocale($locale) {
+        // Set cookie
+        if(!isset($_COOKIE['lang']) || $locale != $_COOKIE['lang'])
+        {
+            setcookie('lang', $locale, time() + SLocale::$cookie_lifetime);
+            putenv("LC_ALL=$locale.UTF-8");
+            setlocale(LC_ALL, "$locale.UTF-8");
+            $_COOKIE['lang'] = $locale;
+        }
+
+        // Set translation file info
+        bindtextdomain('translations', ROOT.'locale');
+        textdomain('translations');
+        bind_textdomain_codeset('translations', 'UTF-8');
+
+        if (!defined('LANG'))
+            define('LANG', $locale);
+    }
+}
+
+?>
--- include/locale.php
+++ include/locale.php
@@ -17,6 +17,7 @@
  * You should have received a copy of the GNU General Public License
  * along with stkaddons.  If not, see <http://www.gnu.org/licenses/>.
  */
+require_once(ROOT.'include/SLocale.class.php');
 
 // Get the current page address (without "lang" parameter)
 $page_url = $_SERVER['REQUEST_URI'];
@@ -26,37 +27,7 @@ if (strstr($page_url,'?') === false)
 $page_url = preg_replace('/lang=[a-z_]+/i',NULL,$page_url);
 $page_url = preg_replace('/[&]+/i','&',$page_url);
 $page_url = preg_replace('/\?&/i','?',$page_url);
-// Time for the language cookie to expire is 1 year in the future
-$timestamp_expire = time() + 365*24*3600;
-// Set language cookie if it is not set
-// Initialize cookie
-if(!isset($_COOKIE['lang']))
-{
-    setcookie('lang', 'en_US', $timestamp_expire);
-    putenv('LC_ALL=en_US.UTF-8');
-    setlocale(LC_ALL, 'en_US.UTF-8');
-    $_COOKIE['lang'] = 'en_US';
-}
-// Set cookie based on request
-if (isset($_GET['lang']))
-{
-    setcookie('lang', $_GET['lang'], $timestamp_expire);
-    putenv('LC_ALL='.$_GET['lang'].'.UTF-8');
-    setlocale(LC_ALL, $_GET['lang'].'.UTF-8');
-    $_COOKIE['lang'] = $_GET['lang'];
-}
-else
-{
-    putenv('LC_ALL='.$_COOKIE['lang'].'.UTF-8');
-    setlocale(LC_ALL, $_COOKIE['lang'].'.UTF-8');
-}
 
-bindtextdomain('translations', ROOT.'locale');
-textdomain('translations');
-bind_textdomain_codeset('translations', 'UTF-8');
-
-/**
- * Language string for the currently configured language 
- */
-define('LANG', $_COOKIE['lang']);
+// Set the locale
+new SLocale();
 ?>
