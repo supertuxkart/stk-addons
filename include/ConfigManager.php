48692e2f1f320bc7c1a4564cf6a45f3567181265
Stephen Just <stephenjust@users.sf.net>
2013-12-29 00:46:49 +0000

    Remove dependency on sql.php from ConfigManager

--- include/ConfigManager.php
+++ include/ConfigManager.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * copyright 2011 Stephen Just <stephenjust@users.sourceforge.net>
+ * Copyright 2011-2013 Stephen Just <stephenjust@users.sourceforge.net>
  *
  * This file is part of stkaddons
  *
@@ -18,6 +18,8 @@
  * along with stkaddons.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+require_once(INCLUDE_DIR . 'DBConnection.class.php');
+
 /**
  * Contains static methods to manage configuration values in database
  */
@@ -27,29 +29,25 @@ class ConfigManager
 
     public static function get_config($config_name) {
         // Validate parameters
-        if (!is_string($config_name)) {
-            return NULL;
-        }
-        if (strlen($config_name) < 1) {
-            return NULL;
-        }
+        if (!is_string($config_name)) return NULL;
+        if (empty($config_name)) return NULL;
 
-        $config_name = mysql_real_escape_string($config_name);
+        $db = DBConnection::get();
 
         // Populate the config cache
         if (count(ConfigManager::$cache == 0)) {
-            $query = 'SELECT `name`, `value`
-                FROM `'.DB_PREFIX.'config`';
-            $handle = sql_query($query);
-            if (!$handle) return NULL;
-
-            $num_config = mysql_num_rows($handle);
-            if ($num_config == 0) {
+            try {
+                $result = $db->query('SELECT `name`, `value`' .
+                                     'FROM `'.DB_PREFIX.'config`',
+                                     DBConnection::FETCH_ALL);
+            } catch (DBException $e) {
+                if (DEBUG_MODE) echo $e->getMessage();
                 return NULL;
             }
-            for ($i = 1; $i <= $num_config; $i++) {
-                $config = sql_next($handle);
-                ConfigManager::$cache[$config['name']] = $config['value'];
+            if (count($result) === 0) return null;
+            
+            foreach ($result AS $row) {
+                ConfigManager::$cache[$row['name']] = $row['value'];
             }
         }
         if (!isset(ConfigManager::$cache[$config_name])) {
@@ -60,36 +58,28 @@ class ConfigManager
 
     public static function set_config($config_name,$config_value) {
             // Validate parameters
-            if (!is_string($config_name)) {
-                    return false;
+            if (!is_string($config_name)) return false;
+            if (strlen($config_name) < 1) return false;
+            if (is_array($config_value)) return false;
+            if (empty($config_value)) return true;  // Not changed because we
+                                                    // can't accept null values
+            $db = DBConnection::get();
+            try {
+                $db->query(
+                        'INSERT INTO `'.DB_PREFIX.'config` '.
+                        '    (`name`, `value`) '.
+                        'VALUES '.
+                        '    (:name, :value) '.
+                        'ON DUPLICATE KEY UPDATE `value` = :value',
+                        DBConnection::NOTHING,
+                        array(
+                            ':name' =>  (string) $config_name,
+                            ':value' => (string) $config_value
+                        ));
+            } catch (DBException $e) {
+                if (DEBUG_MODE) echo $e->getMessage();
+                return false;
             }
-            if (strlen($config_name) < 1) {
-                    return false;
-            }
-            if (is_array($config_value)) {
-                    return false;
-            }
-            if (strlen($config_value) < 1) {
-                    return true; // Not changed because we can't accept null values
-            }
-
-            $config_name = mysql_real_escape_string($config_name);
-            $config_value = mysql_real_escape_string($config_value);
-
-            // Check if value already exists in database
-            if (!is_null(ConfigManager::get_config($config_name))) {
-                    // Value exists
-                    $set_query = "UPDATE `".DB_PREFIX."config`
-                            SET `value` = '$config_value'
-                            WHERE `name` = '$config_name'";
-            } else {
-                    // Value does not exist
-                    $set_query = "INSERT INTO `".DB_PREFIX."config`
-                            (`name`,`value`)
-                            VALUES ('$config_name','$config_value')";
-            }
-            $set_handle = sql_query($set_query);
-            if (!$set_handle) return false;
 
             // Update cache - first, make sure the cache exists
             ConfigManager::get_config($config_name);
