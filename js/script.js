199dc07eddab36c10d8e0375704bfdd1f33d5b67
Stephen Just <stephenjust@users.sf.net>
2014-03-08 15:00:02 +0000

    Remove some unused javascript code
    This is still in dire need of being rewritten.

--- js/script.js
+++ js/script.js
@@ -13,7 +13,7 @@ function confirm_delete(url)
 function loadSub(newSub)
 {
     newSub = "sub" + newSub;
-    if(oldSub !="")	document.getElementById(oldSub).style.display = "none";
+    if(oldSub !== "")	document.getElementById(oldSub).style.display = "none";
     document.getElementById(newSub).style.display = "block";
     oldSub = newSub;
 }
@@ -30,7 +30,6 @@ function loadFrame(id, page, value)
     function(data) {
         $("#right-content_body").html(data);
         $("#right-content_body").scrollTop(0);
-        load_jquery();
     });
 }
 
@@ -40,37 +39,16 @@ function addonRequest(page, id, value)
     function(data){
         $("#content-addon_body").html(data);
         $("#content-addon_body").scrollTop(0);
-        load_jquery();
     });
 }
 function loadDiv(newDiv)
 {
     newDiv = "disp" + newDiv;
-    if(oldDiv !="")	document.getElementById(oldDiv).style.display = "none";
+    if(oldDiv !== "")	document.getElementById(oldDiv).style.display = "none";
     document.getElementById(newDiv).style.display = "block";
     oldDiv = newDiv;
     document.getElementById("content-addon_body").innerHTML ="";
     document.getElementById("content-addon_body").style.display="none";
-}
-function changeClassSub(obj)
-{
-    if(oldElSub !="") oldElSub.className="sub";
-    obj.className = "subSelected";
-    oldElSub=obj;
-}
-function changeClassRoot(obj)
-{
-    if(oldRoot !="")oldRoot.className="root";
-    obj.className = "rootSelected";
-    oldRoot=obj;
-}
-function verify(codeSent)
-{
-    if (confirm("Do you want remove this add-ons") == true)
-    {
-        eval(codeSent);
-        location.reload();
-    }
 }
 
 function clearPanelStatus() {
@@ -83,18 +61,12 @@ $(document).ready(function () {
     $('#lang-menu > a').click(function () {
         $('ul.menu_body').slideToggle('medium');
     });
-    $('#advance_button').click(function () {
-    //if($("#advanced").css("margin-right")!= "279px") $("#advanced").animate({ marginRight: "279px" }, 150 );
-    //else  $("#advanced").animate({ marginRight: "1px" }, 150 );
-    if($("#content_advanced").css("width")!= "300px") $("#content_advanced").animate({width: "300px"}, 150 );
-    else  $("#content_advanced").animate({width: "0px"}, 150 );
-    });
 
     $('a.addon-list').click(function() {
       history.pushState({path: this.path}, '', this.href);
       var url = this.href;
       var addonType = getUrlVars(url)['type'];
-      if (addonType == undefined) {
+      if (addonType === undefined) {
 	  url = siteRoot + $(this).children('meta').attr("content").replace('&amp;','&');
 	  addonType = getUrlVars(url)['type'];
       }
@@ -123,24 +95,12 @@ $(document).ready(function () {
     });
 });
 
-function load_jquery()
-{
-    $("div.help-hidden").click(function () {
-        $(this).children("div").slideToggle('medium');
-    });
-    $("span.help-hidden").mouseover(function () {
-        document.body.style.cursor='pointer';
-    });
-    $("span.help-hidden").mouseout(function () {
-        document.body.style.cursor='default';
-    });
-}
-
 function textLimit(field, num) {
     if (field.value.length > num) {
         field.value = field.value.substring(0, num);
     }
 }
+
 function addRating(rating,addonId,sel_storage,disp_storage) {
     loadHTML(siteRoot + 'include/addRating.php?rating='+encodeURI(rating)+'&addonId='+encodeURI(addonId),sel_storage);
     loadHTML(siteRoot + 'include/addRating.php?addonId='+encodeURI(addonId),disp_storage);
@@ -179,9 +139,9 @@ function loadHTML(url, storage)
     var xhr = createXHR();
     xhr.onreadystatechange=function()
     {
-        if(xhr.readyState == 4)
+        if(xhr.readyState === 4)
         {
-            if (storage_elem.innerHTML == "undefined")
+            if (storage_elem.innerHTML === undefined)
             {
                 storage_elem = xhr.responseText;
             } else {
