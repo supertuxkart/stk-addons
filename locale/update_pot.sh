25845398ffa4ad74d60cc9d785cbf743cdd21350
Daniel Butum <danibutum@gmail.com>
2014-05-10 18:20:22 +0300

    Minor refactoring and removal of Util class

--- locale/update_pot.sh
+++ locale/update_pot.sh
@@ -2,6 +2,12 @@
 rm file-list.txt
 ls ../*.php >> ./file-list.txt
 ls ../include/*.php >> ./file-list.txt
-xgettext --language=php --output=./translations.pot --msgid-bugs-address=stephenjust@users.sourceforge.net \
---add-comments=I18N --copyright-holder="SuperTuxKart Team" --package-name=stkaddons -Fn \
+xgettext \
+--language=php \
+--keyword=_h \
+--output=./translations.pot \
+--msgid-bugs-address=stephenjust@users.sourceforge.net \
+--add-comments=I18N \
+--copyright-holder="SuperTuxKart Team" \
+--package-name=stkaddons -Fn \
 --files-from=file-list.txt
