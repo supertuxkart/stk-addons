60ed03597bdc2adc4624781aa96c6fc25372739c
Daniel Butum <danibutum@gmail.com>
2014-05-15 19:31:36 +0300

    Update generate locale script to include the new smarty plugin

--- locale/update_pot.sh
+++ locale/update_pot.sh
@@ -1,13 +1,26 @@
 #!/bin/bash
-rm file-list.txt
+rm -f file-list.txt
 ls ../*.php >> ./file-list.txt
 ls ../include/*.php >> ./file-list.txt
+
+echo "Generating from code"
 xgettext \
 --language=php \
 --keyword=_h \
---output=./translations.pot \
+--output=./code.pot \
 --msgid-bugs-address=stephenjust@users.sourceforge.net \
 --add-comments=I18N \
 --copyright-holder="SuperTuxKart Team" \
 --package-name=stkaddons -Fn \
 --files-from=file-list.txt
+
+echo "Generating from templates"
+./tsmarty2c.php -o template.pot ../tpl/
+
+echo "Concatenating"
+msgcat -o translations.pot code.pot template.pot
+
+echo "Cleaning up"
+rm -f code.pot template.pot
+
+echo "DONE"
