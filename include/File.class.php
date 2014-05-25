b66c4da7795eb4d5ca62f6ad295ea9c33bac91c6
Daniel Butum <danibutum@gmail.com>
2014-05-25 17:25:21 +0300

    Fix 'sprintf(): Too few arguments in'

--- include/File.class.php
+++ include/File.class.php
@@ -527,7 +527,7 @@ class File
         {
             if (DEBUG_MODE)
             {
-                throw new FileException(sprintf("%s is not a directory"), $dir);
+                throw new FileException(sprintf("%s is not a directory", $dir));
             }
 
             return null;
