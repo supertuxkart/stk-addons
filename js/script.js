3c3a1715da3425b32621802c3e7e1b265f7353f4
Daniel Butum <danibutum@gmail.com>
2014-05-26 22:32:56 +0300

    Remove old xhr method use jquery

--- js/script.js
+++ js/script.js
@@ -3,58 +3,6 @@ var oldSub = "";
 var oldRoot = "";
 var oldDiv = "";
 
-function confirm_delete(url) {
-    if (confirm("Really delete this item?")) {
-        window.location = url;
-    }
-}
-
-function loadSub(newSub) {
-    newSub = "sub" + newSub;
-    if (oldSub !== "") {
-        document.getElementById(oldSub).style.display = "none";
-    }
-    document.getElementById(newSub).style.display = "block";
-    oldSub = newSub;
-}
-
-function loadAddon(id, page) {
-    addonRequest(page, id);
-}
-
-function loadFrame(id, page, value) {
-    var panelDiv = document.getElementById('right-content_body');
-    panelDiv.innerHTML = '<div id="loading"></div>';
-    $.get(page, {id: id, value: value},
-        function (data) {
-            $("#right-content_body").html(data);
-            $("#right-content_body").scrollTop(0);
-        }
-    );
-}
-
-function addonRequest(page, id, value) {
-    $.post(page, {id: id, value: value},
-        function (data) {
-            $("#content-addon_body").html(data);
-            $("#content-addon_body").scrollTop(0);
-        }
-    );
-}
-function loadDiv(newDiv) {
-    newDiv = "disp" + newDiv;
-    if (oldDiv !== "")    document.getElementById(oldDiv).style.display = "none";
-    document.getElementById(newDiv).style.display = "block";
-    oldDiv = newDiv;
-    document.getElementById("content-addon_body").innerHTML = "";
-    document.getElementById("content-addon_body").style.display = "none";
-}
-
-function clearPanelStatus() {
-    var div = document.getElementById('right-content_status');
-    div.innerHTML = '';
-}
-
 $(document).ready(function () {
     $("#news-messages").newsTicker();
     $('#lang-menu > a').click(function () {
@@ -94,6 +42,49 @@ $(document).ready(function () {
     });
 });
 
+function confirm_delete(url) {
+    if (confirm("Really delete this item?")) {
+        window.location = url;
+    }
+}
+
+function loadAddon(id, page) {
+    addonRequest(page, id);
+}
+
+function loadFrame(id, page, value) {
+    var panelDiv = document.getElementById('right-content_body');
+    panelDiv.innerHTML = '<div id="loading"></div>';
+    $.get(page, {id: id, value: value},
+        function (data) {
+            $("#right-content_body").html(data);
+            $("#right-content_body").scrollTop(0);
+        }
+    );
+}
+
+function addonRequest(page, id, value) {
+    $.post(page, {id: id, value: value},
+        function (data) {
+            $("#content-addon_body").html(data);
+            $("#content-addon_body").scrollTop(0);
+        }
+    );
+}
+function loadDiv(newDiv) {
+    newDiv = "disp" + newDiv;
+    if (oldDiv !== "")    document.getElementById(oldDiv).style.display = "none";
+    document.getElementById(newDiv).style.display = "block";
+    oldDiv = newDiv;
+    document.getElementById("content-addon_body").innerHTML = "";
+    document.getElementById("content-addon_body").style.display = "none";
+}
+
+function clearPanelStatus() {
+    var div = document.getElementById('right-content_status');
+    div.innerHTML = '';
+}
+
 function textLimit(field, num) {
     if (field.value.length > num) {
         field.value = field.value.substring(0, num);
@@ -101,25 +92,10 @@ function textLimit(field, num) {
 }
 
 function addRating(rating, addonId, sel_storage, disp_storage) {
-    loadHTML(siteRoot + 'include/addRating.php?rating=' + encodeURI(rating) + '&addonId=' + encodeURI(addonId),
-        sel_storage);
+    loadHTML(siteRoot + 'include/addRating.php?rating=' + encodeURI(rating) + '&addonId=' + encodeURI(addonId), sel_storage);
     loadHTML(siteRoot + 'include/addRating.php?addonId=' + encodeURI(addonId), disp_storage);
 }
 
-// AJAX functions
-function createXHR() {
-    var xmlhttp = false;
-    try {// code for IE7+, Firefox, Chrome, Opera, Safari
-        xmlhttp = new XMLHttpRequest();
-    } catch (e) {// code for IE6, IE5
-        try {
-            xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
-        } catch (e) {
-            xmlhttp = false;
-        }
-    }
-    return xmlhttp;
-}
 /**
  * Loads an HTML page
  * Put the content of the body tag into the current page.
@@ -128,18 +104,13 @@ function createXHR() {
  */
 function loadHTML(url, storage) {
     var storage_elem = document.getElementById(storage);
-    var xhr = createXHR();
-    xhr.onreadystatechange = function () {
-        if (xhr.readyState === 4) {
-            if (storage_elem.innerHTML === undefined) {
-                storage_elem = xhr.responseText;
-            } else {
-                storage_elem.innerHTML = xhr.responseText;
-            }
+    $.get(url, function(data) {
+        if (storage_elem.innerHTML === undefined) {
+            storage_elem = data;
+        } else {
+            storage_elem.innerHTML = data;
         }
-    };
-    xhr.open("GET", url, true);
-    xhr.send(null);
+    });
 }
 
 // Read a page's GET URL variables and return them as an associative array.
