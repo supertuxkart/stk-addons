1b25d83d25cd7a9024542c6bde646fa6c618778a
Daniel Butum <danibutum@gmail.com>
2014-06-01 19:58:20 +0300

    Remove old jQuery Clean reports class

--- include/Report.class.php
+++ include/Report.class.php
@@ -20,12 +20,26 @@
 
 require_once(INCLUDE_PATH . 'graphs.php');
 
-class Report 
+/**
+ * Class Report
+ */
+class Report
 {
+    /**
+     * @var string
+     */
     private $report_title;
-    private $report_description = NULL;
+
+    /**
+     * @var null
+     */
+    private $report_description = null;
+
+    /**
+     * @var array
+     */
     private $report_structure = array();
-    
+
     /**
      * Constructor
      */
@@ -33,201 +47,250 @@ class Report
     {
         $this->report_title = $title;
     }
-    
+
     /**
      * Add a report description
+     *
      * @param string $description Report description in HTML format
      */
     public function addDescription($description)
     {
         $this->report_description = $description;
     }
-    
+
     /**
      * Add a section to the current report
+     *
      * @param string $name Section heading
+     *
      * @return int Section id
      */
     public function addSection($name)
     {
-        $this->report_structure[] = array('name' => $name, 'content' => NULL);
+        $this->report_structure[] = array('name' => $name, 'content' => null);
         end($this->report_structure);
+
         return key($this->report_structure);
     }
-    
-    public function addQuery($section,$query)
+
+    /**
+     * @param $section
+     * @param $query
+     */
+    public function addQuery($section, $query)
     {
         $db = DBConnection::get();
-        
+
         $query_result = "\t<h3>Query</h3>\n";
-        $query_result .= "\t<code>".htmlspecialchars($query)."</code>\n";
-        try {
+        $query_result .= "\t<code>" . htmlspecialchars($query) . "</code>\n";
+        try
+        {
             $result = $db->query($query, DBConnection::FETCH_ALL);
             $count = count($result);
-        } catch (DBException $e) {
-            $this->report_structure[$section]['content'] .= $query_result.'<p>ERROR.</p>';
+        }
+        catch(DBException $e)
+        {
+            $this->report_structure[$section]['content'] .= $query_result . '<p>ERROR.</p>';
+
             return;
         }
         $query_result .= "\t<h3>Result</h3>\n";
         $query_result .= "\t<p>($count rows returned)</p>\n";
-        
+
         // Don't draw the table if we have no results
-        if ($count === 0) {
+        if ($count === 0)
+        {
             $this->report_structure[$section]['content'] .= $query_result;
+
             return;
         }
-        
+
         $query_result .= "\t<table cellspacing=\"0\" class=\"sortable\"><thead>\n\t\t<tr>\n";
         // List column names
         $column_names = array_keys($result[0]);
-        foreach ($column_names AS $col) {
+        foreach ($column_names AS $col)
+        {
             $query_result .= "\t\t\t<th>$col</th>\n";
         }
         $query_result .= "\t\t</tr></thead><tbody>\n";
-        
+
         // List results
-        foreach ($result AS $row) {
+        foreach ($result AS $row)
+        {
             $query_result .= "\t\t<tr>\n";
-            foreach ($column_names AS $col) {
+            foreach ($column_names AS $col)
+            {
                 $current_result = htmlspecialchars($row[$col]);
                 $query_result .= "\t\t\t<td>{$current_result}</td>\n";
             }
             $query_result .= "\t\t</tr>\n";
         }
-        
+
         $query_result .= "\t</tbody></table>";
-        
+
         $this->report_structure[$section]['content'] .= $query_result;
     }
-    
+
     /**
      * Inserts a pie chart into the report. The query must be formed such that
      * the first column returned is a label, and the second is a numerical
      * value.
+     *
      * @param string $section
      * @param string $query
+     * @param string $chartTitle
      * @param string $graphId
-     * @return void
      */
-    public function addPieChart($section,$query,$chartTitle,$graphId = NULL) {
+    public function addPieChart($section, $query, $chartTitle, $graphId = null)
+    {
         $db = DBConnection::get();
-        
+
         $query_result = "\t<h3>Pie Chart</h3>\n";
-        $query_result .= "\t<code>".htmlspecialchars($query)."</code>\n";
-        try {
+        $query_result .= "\t<code>" . htmlspecialchars($query) . "</code>\n";
+        try
+        {
             $result = $db->query($query, DBConnection::FETCH_ALL);
             $count = count($result);
-        } catch (DBException $e) {
-            $this->report_structure[$section]['content'] .= $query_result.'<p>ERROR.</p>';
+        }
+        catch(DBException $e)
+        {
+            $this->report_structure[$section]['content'] .= $query_result . '<p>ERROR.</p>';
+
             return;
         }
         $query_result .= "\t<h3>Result</h3>\n";
         $query_result .= "\t<p>($count rows returned)</p>\n";
-        
-        if ($count === 0) {
+
+        if ($count === 0)
+        {
             $this->report_structure[$section]['content'] .= $query_result;
+
             return;
         }
-        
+
         $col_names = array_keys($result[0]);
-        if (count($col_names) !== 2) {
-            $this->report_structure[$section]['content'] .= "<p>Error: This query did not return a result with two columns.</p>";
+        if (count($col_names) !== 2)
+        {
+            $this->report_structure[$section]['content'] .=
+                "<p>Error: This query did not return a result with two columns.</p>";
         }
 
         $values = array();
         $labels = array();
-        foreach ($result AS $row) {
+        foreach ($result AS $row)
+        {
             $labels[] = $row[$col_names[0]];
             $values[] = $row[$col_names[1]];
         }
-        
+
         $data_file = graph_data_to_json($values, $labels, 'pie', $graphId);
-        $query_result .= '<div class="pie_chart" id="'.$graphId.'">'.$chartTitle."\n".$data_file.'</div>';
+        $query_result .= '<div class="pie_chart" id="' . $graphId . '">' . $chartTitle . "\n" . $data_file . '</div>';
 
         $this->report_structure[$section]['content'] .= $query_result;
     }
-    
+
     /**
      * Inserts a graph into the report. The query must be formed such that
      * the first column returned is a label, the second is a date, and the third
      * is a numerical value. This function can handle graphs with multiple
      * lines.
-     * @param type $section
-     * @param type $query
-     * @return type 
+     *
+     * @param string $section
+     * @param string $query
+     * @param string $chartTitle
+     * @param string|null $graphId
      */
-    public function addTimeGraph($section, $query, $chartTitle, $graphId = NULL) {
+    public function addTimeGraph($section, $query, $chartTitle, $graphId = null)
+    {
         $db = DBConnection::get();
-        
+
         $query_result = "\t<h3>Graph</h3>\n";
-        $query_result .= "\t<code>".htmlspecialchars($query)."</code>\n";
-        try {
+        $query_result .= "\t<code>" . htmlspecialchars($query) . "</code>\n";
+        try
+        {
             $points = $db->query($query, DBConnection::FETCH_ALL);
             $count = count($points);
-        } catch (DBException $e) {
-            $this->report_structure[$section]['content'] .= $query_result.'<p>ERROR.</p>';
+        }
+        catch(DBException $e)
+        {
+            $this->report_structure[$section]['content'] .= $query_result . '<p>ERROR.</p>';
+
             return;
         }
         $query_result .= "\t<h3>Result</h3>\n";
         $query_result .= "\t<p>($count rows returned)</p>\n";
-        if ($count === 0) {
+        if ($count === 0)
+        {
             $this->report_structure[$section]['content'] .= $query_result;
+
             return;
         }
 
         $col_names = array_keys($points[0]);
-        
+
         // Group points by label
         $lines = array();
-        foreach ($points AS $point) {
+        foreach ($points AS $point)
+        {
             // Hash the line label in the key to remove bad characters
-            if (!array_key_exists(md5($point[$col_names[0]]),$lines))
-                $lines[md5($point[$col_names[0]])] = array('label' => $point[$col_names[0]], 'x' => array(), 'y' => array());
-            
+            if (!array_key_exists(md5($point[$col_names[0]]), $lines))
+            {
+                $lines[md5($point[$col_names[0]])] =
+                    array('label' => $point[$col_names[0]], 'x' => array(), 'y' => array());
+            }
+
             $lines[md5($point[$col_names[0]])]['x'][] = $point[$col_names[1]];
             $lines[md5($point[$col_names[0]])]['y'][] = $point[$col_names[2]];
         }
         $lines = array_values($lines);
-        
+
         // Split into arrays of x-values, y-values and labels
         $xvalues = array();
         $yvalues = array();
         $labels = array();
-        foreach ($lines AS $line) {
+        foreach ($lines AS $line)
+        {
             $xvalues[] = $line['x'];
             $yvalues[] = $line['y'];
             $labels[] = $line['label'];
         }
         // Make x-values numeric
-        for ($i = 0; $i < count($xvalues); $i++) {
+        for ($i = 0; $i < count($xvalues); $i++)
+        {
             for ($j = 0; $j < count($xvalues[$i]); $j++)
+            {
                 $xvalues[$i][$j] = strtotime($xvalues[$i][$j]);
+            }
         }
-        
+
         $data_file = graph_data_to_json(array($xvalues, $yvalues), $labels, 'time', $graphId);
-        $query_result .= '<div class="time_chart" id="'.$graphId.'">'.$chartTitle."\n".$data_file.'</div>';
+        $query_result .= '<div class="time_chart" id="' . $graphId . '">' . $chartTitle . "\n" . $data_file . '</div>';
 
         $this->report_structure[$section]['content'] .= $query_result;
     }
-    
+
+    /**
+     * @return string
+     */
     public function __toString()
     {
         $return = "<html>\n<head>\n\t<title>{$this->report_title}</title>\n";
-        $return .= "\t<script src=\"".ROOT."js/sorttable.js\" type=\"text/javascript\"></script>\n";
+        $return .= "\t<script src=\"" . ROOT . "js/sorttable.js\" type=\"text/javascript\"></script>\n";
         $return .= "\t<script src=\"https://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js\" type=\"text/javascript\"></script>\n";
         $return .= "\t<script src=\"https://www.google.com/jsapi\" type=\"text/javascript\"></script>\n";
-        $return .= "\t<script src=\"".ROOT."js/reports.js\" type=\"text/javascript\"></script>\n";
-        $return .= "\t<link href=\"".ROOT."css/report.css\" rel=\"StyleSheet\" type=\"text/css\" />\n</head>\n";
+        $return .= "\t<script src=\"" . ROOT . "js/reports.js\" type=\"text/javascript\"></script>\n";
+        $return .= "\t<link href=\"" . ROOT . "css/report.css\" rel=\"StyleSheet\" type=\"text/css\" />\n</head>\n";
         $return .= "<body>\n\t<h1>{$this->report_title}</h1>\n";
         $return .= "\t<blockquote>{$this->report_description}</blockquote>\n";
-        
+
         foreach ($this->report_structure AS $section)
         {
             $return .= "\t<h2>{$section['name']}</h2>\n";
             $return .= "\t<blockquote>{$section['content']}</blockquote>\n";
         }
-        
+
         $return .= "</body>\n</html>";
+
         return $return;
     }
 }
--- reports/files.php
+++ reports/files.php
@@ -22,47 +22,47 @@ require_once(dirname(__DIR__) . DIRECTORY_SEPARATOR . "config.php");
 
 $report = new Report("STK Add-Ons File Report");
 $description = '<p>This report contains a list of all addon package files, and '
- .'their download counts. Add-ons that appear more than once in each list have '
- .'more than one revision or more than one file uploaded. Files with blank '
- .'dates were uploaded before that field was added.</p>';
+    . 'their download counts. Add-ons that appear more than once in each list have '
+    . 'more than one revision or more than one file uploaded. Files with blank '
+    . 'dates were uploaded before that field was added.</p>';
 $report->addDescription($description);
 
 $chart_section = $report->addSection("File Downloads (by add-on type)");
 $chart_query = 'SELECT `addon_type`,SUM(`downloads`)
-    FROM `'.DB_PREFIX.'files`
+    FROM `' . DB_PREFIX . 'files`
     WHERE `file_type` = \'addon\'
     GROUP BY `addon_type`';
-$report->addPieChart($chart_section, $chart_query, 'Downloads by Type','files_pie');
+$report->addPieChart($chart_section, $chart_query, 'Downloads by Type', 'files_pie');
 
 $addon_section = $report->addSection("Add-Ons (by revision)");
 $addon_query = 'SELECT `addon_id`,`addon_type`,`file_path`,`date_added`,`downloads`
-    FROM `'.DB_PREFIX.'files`
+    FROM `' . DB_PREFIX . 'files`
     WHERE `file_type` = \'addon\'
     ORDER BY `addon_id` ASC, `date_added` ASC';
-$report->addQuery($addon_section,$addon_query);
+$report->addQuery($addon_section, $addon_query);
 
 $dl_section = $report->addSection("Add-Ons Cumulative Downloads");
 $dl_query = 'SELECT `a`.`id`,`a`.`type`,`a`.`name`,
     sum(`f`.`downloads`) AS `dl_count`
-    FROM `'.DB_PREFIX.'addons` `a`, `'.DB_PREFIX.'files` `f`
+    FROM `' . DB_PREFIX . 'addons` `a`, `' . DB_PREFIX . 'files` `f`
     WHERE `a`.`id` = `f`.`addon_id`
     AND `f`.`file_type` = \'addon\'
     GROUP BY `a`.`id`
     ORDER BY `a`.`id` ASC';
-$report->addQuery($dl_section,$dl_query);
+$report->addQuery($dl_section, $dl_query);
 
 $image_section = $report->addSection("Images");
 $image_query = 'SELECT `addon_id`,`addon_type`,`file_path`,`date_added`,`approved`,`downloads`
-    FROM `'.DB_PREFIX.'files`
+    FROM `' . DB_PREFIX . 'files`
     WHERE `file_type` = \'image\'
     ORDER BY `addon_id` ASC, `date_added` ASC';
-$report->addQuery($image_section,$image_query);
+$report->addQuery($image_section, $image_query);
 
 $source_section = $report->addSection("Source");
 $source_query = 'SELECT `addon_id`,`addon_type`,`file_path`,`date_added`,`approved`,`downloads`
-    FROM `'.DB_PREFIX.'files`
+    FROM `' . DB_PREFIX . 'files`
     WHERE `file_type` = \'source\'
     ORDER BY `addon_id` ASC, `date_added` ASC';
-$report->addQuery($source_section,$source_query);
+$report->addQuery($source_section, $source_query);
 
-print($report);
+echo $report;
