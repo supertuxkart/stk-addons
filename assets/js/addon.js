b15a3ae5f36eca3625421598ab2f3c8e57239694
Daniel Butum <danibutum@gmail.com>
2016-10-21 10:30:21 +0300

    Change GPL license header to use modern english and proper punctuation

--- assets/js/addon.js
+++ assets/js/addon.js
@@ -2,20 +2,20 @@
  * copyright 2013      Stephen Just <stephenjust@gmail.com>
  *           2014-2015 Daniel Butum <danibutum at gmail dot com>
  *
- * This file is part of stkaddons
+ * This file is part of stk-addons.
  *
- * stkaddons is free software: you can redistribute it and/or modify
+ * stk-addons is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * stkaddons is distributed in the hope that it will be useful,
+ * stk-addons is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with stkaddons.  If not, see <http://www.gnu.org/licenses/>.
+ * along with stk-addons. If not, see <http://www.gnu.org/licenses/>.
  */
 
 (function($, ROOT_LOCATION, JSON_LOCATION) {
