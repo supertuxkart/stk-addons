5de300592a210c0cdac198015e07c47ebda72723
Adam Žurek <thejabko@gmail.com>
2018-07-11 00:11:11 +0200

    cron classes reformatted to space indented

--- cron/src/DailyCron.php
+++ cron/src/DailyCron.php
@@ -2,27 +2,27 @@
 
 final class DailyCron
 {
-	public static function run(): void
-	{
-		try
-		{
-			echo File::deleteQueuedFiles() . "\n";
-			writeXML();
-			echo "SUCCESS: File::deleteQueuedFiles \n";
-		}
-		catch (FileException $e)
-		{
-			echo "ERROR: File::deleteQueuedFiles \n" . $e->getMessage();
-		}
+    public static function run(): void
+    {
+        try
+        {
+            echo File::deleteQueuedFiles() . "\n";
+            writeXML();
+            echo "SUCCESS: File::deleteQueuedFiles \n";
+        }
+        catch (FileException $e)
+        {
+            echo "ERROR: File::deleteQueuedFiles \n" . $e->getMessage();
+        }
 
-		try
-		{
-			Verification::cron(7);
-			echo "SUCCESS: Verification::cron \n";
-		}
-		catch (VerificationException $e)
-		{
-			echo "ERROR: Verification::cron \n" . $e->getMessage();
-		}
-	}
+        try
+        {
+            Verification::cron(7);
+            echo "SUCCESS: Verification::cron \n";
+        }
+        catch (VerificationException $e)
+        {
+            echo "ERROR: Verification::cron \n" . $e->getMessage();
+        }
+    }
 }
