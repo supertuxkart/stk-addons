5b50bfe1bc959e5922315c01e81680cb1810f743
Adam Žurek <thejabko@gmail.com>
2018-07-10 16:51:33 +0200

    cron classes: reformatted

--- cron/src/HourlyCron.php
+++ cron/src/HourlyCron.php
@@ -2,13 +2,16 @@
 
 final class HourlyCron
 {
-    public static function run(): void
-    {
-        try {
-            ClientSession::cron(5 * 60 /* 5 minutes */, 3600 * 24 * 30 /* 1 month */);
-            echo "SUCCESS \n";
-        } catch (ClientSessionException $e) {
-            echo "ERROR: \n" . $e->getMessage();
-        }
-    }
+	public static function run(): void
+	{
+		try
+		{
+			ClientSession::cron(5 * 60 /* 5 minutes */, 3600 * 24 * 30 /* 1 month */);
+			echo "SUCCESS \n";
+		}
+		catch (ClientSessionException $e)
+		{
+			echo "ERROR: \n" . $e->getMessage();
+		}
+	}
 }
