85a65ce5aa2ec189572e81b20ad34333f7479b7a
auria.mg <auria.mg@gmail.com>
2018-08-26 20:48:08 -0400

    Add script (.as) files to the list of allowed files

--- install/install.sql
+++ install/install.sql
@@ -674,7 +674,7 @@ CREATE TABLE IF NOT EXISTS `v3_config` (
 --
 INSERT INTO `v3_config` (`name`, `value`) VALUES
     ('allowed_addon_exts', 'b3d, bz2, gz, jpeg, jpg, music, ogg, png, spm, tar, tar.bz2, tar.gz, tbz, tgz, txt, xml, zip'),
-    ('allowed_source_exts', 'b3d, blend, jpeg, jpg, music, ogg, png, rgb, spm, svg, txt, xcf, xml'),
+    ('allowed_source_exts', 'as, b3d, blend, jpeg, jpg, music, ogg, png, rgb, spm, svg, txt, xcf, xml'),
     ('max_image_dimension', '2048'),
     ('blog_feed', 'http://supertuxkart.blogspot.com/feeds/posts/default')
 ON DUPLICATE KEY UPDATE `name` = VALUES(`name`), `value` = VALUES(`value`);
