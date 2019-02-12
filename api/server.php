8ff04c06997c6c32434a171dcfc576620f58f3b4
Benau <Benau@users.noreply.github.com>
2019-02-12 10:48:01 +0800

    Remove utf8_encode for name in create server request
    This way we can store utf8 server name in database, and the xmlWriter
    will do the encode for the get-all request anyway
    
    And for the record utf8_encode is only needed if the originally string
    is encoded in iso88591

--- api/server.php
+++ api/server.php
@@ -38,7 +38,7 @@ try
                 $address = isset($_POST['address']) ? utf8_encode($_POST['address']) : null;
                 $port = isset($_POST['port']) ? (int)utf8_encode($_POST['port']) : null;
                 $private_port = isset($_POST['private_port']) ? (int)utf8_encode($_POST['private_port']) : null;
-                $server_name = isset($_POST['name']) ? utf8_encode($_POST['name']) : "";
+                $server_name = isset($_POST['name']) ? $_POST['name'] : "";
                 $max_players = isset($_POST['max_players']) ? (int)$_POST['max_players'] : 0;
                 $difficulty = isset($_POST['difficulty']) ? (int)$_POST['difficulty'] : 0;
                 $game_mode = isset($_POST['game_mode']) ? (int)$_POST['game_mode'] : 0;
